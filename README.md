4e9f563d99d2c322c31a0718841ab0f052cec2d0
Adnan <adnantium@gmail.com>
2019-03-09 14:34:05 -0500

    Added Kitty struct and KittyStorage trait

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# substrate-package
+# substratekitties
+
+
+
+'''
+$ build.sh
+$ mkdir substratekitties_chain
+$ ./substratekitties/target/release/substratekitties -d ./substratekitties_chain --dev
+'''
+
+
--- substratekitties/runtime/src/lib.rs
+++ substratekitties/runtime/src/lib.rs
@@ -47,7 +47,7 @@ pub type BlockNumber = u64;
 pub type Nonce = u64;
 
 /// Used for the module template in `./template.rs`
-mod template;
+mod substratekitties;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -174,9 +174,11 @@ impl sudo::Trait for Runtime {
 }
 
 /// Used for the module template in `./template.rs`
-impl template::Trait for Runtime { 
-	type Event = Event;
-}
+// impl template::Trait for Runtime { 
+// 	type Event = Event;
+// }
+
+impl substratekitties::Trait for Runtime {}
 
 construct_runtime!(
 	pub enum Runtime with Log(InternalLog: DigestItem<Hash, Ed25519AuthorityId>) where
@@ -193,7 +195,8 @@ construct_runtime!(
 		Sudo: sudo,
 		Fees: fees::{Module, Storage, Config<T>, Event<T>},
 		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		// TemplateModule: template::{Module, Call, Storage, Event<T>},
+		Substratekitties: substratekitties::{Module, Call, Storage},
 	}
 );
 
