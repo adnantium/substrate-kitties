079639c0ff437196f517060f14d92b1f63733a47
Adnan <adnantium@gmail.com>
2019-03-11 10:37:01 -0400

    Added Kitties, KittyOwner and OwnedKitty maps

--- substratekitties/README.md
+++ substratekitties/README.md
@@ -1,3 +1,47 @@
 # substratekitties
 
 A new SRML-based Substrate node, ready for hacking.
+
+
+
+
+### Directory Structure
+
+```
+.
+├── Cargo.lock          The top level rust/cargo package version locks
+├── Cargo.toml          The top level rust/cargo config
+├── LICENSE             See http://unlicense.org
+├── README.md           
+├── build.rs
+├── build.sh
+├── init.sh             Inits WASM env install/updates rust and wasm-gc
+├── runtime             The code runtime code for this node
+│   ├── Cargo.toml      Runtime's rust/cargo config
+│   ├── src\
+│   │   ├─ lib.rs       
+│   │   └─ template.rs
+│   └── wasm
+│       ├─ Cargo.lock
+│       ├─ Cargo.toml
+│       ├─ build.sh
+│       └─ src
+│           └── lib.rs
+├── src
+│   ├─ chain_spec.rs
+│   ├─ cli.rs
+│   ├─ error.rs
+│   ├─ main.rs
+│   └─ service.rs
+├── target
+    └─ debug/
+    └─ release/
+│
+
+```
+
+
+
+
+----
+
--- substratekitties/runtime/src/substratekitties.rs
+++ substratekitties/runtime/src/substratekitties.rs
@@ -1,4 +1,5 @@
-use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
+use support::{decl_storage, decl_module, StorageValue, StorageMap,
+    dispatch::Result, ensure};
 use system::ensure_signed;
 use runtime_primitives::traits::{As, Hash};
 use parity_codec::{Encode, Decode};
@@ -15,24 +16,35 @@ pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        OwnedKitty: map T::AccountId => Kitty<T::Hash, T::Balance>;
+        Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
+        KittyOwner get(kitty_owner): map T::Hash => Option<T::AccountId>;
+        OwnedKitty get(kitty_of_owner): map T::AccountId => T::Hash;
+        Nonce: u64;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-
         fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
+            let nonce = <Nonce<T>>::get();
+            let random_seed = <system::Module<T>>::random_seed();
+            let random_hash = (random_seed, sender.clone(), nonce).using_encoded(<T as system::Trait>::Hashing::hash);
+            ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
+
             let new_kitty = Kitty {
-                id: <T as system::Trait>::Hashing::hash_of(&0),
-                dna: <T as system::Trait>::Hashing::hash_of(&0),
+                id: random_hash,
+                dna: random_hash,
                 price: <T::Balance as As<u64>>::sa(0),
                 gen: 0,
             };
 
-            <OwnedKitty<T>>::insert(&sender, new_kitty);
+            <Kitties<T>>::insert(new_kitty.id, &new_kitty);
+            <KittyOwner<T>>::insert(new_kitty.id, &sender);
+            <OwnedKitty<T>>::insert(&sender, new_kitty.id);
+
+            <Nonce<T>>::mutate(|n| *n += 1);
 
             Ok(())
         }
